42237a26c83bca4be269cdf1cb934fd226dc4eab
valmojr <val_mo_jr@hotmail.com>
2023-06-03 03:07:03 -0300

    added label handing in embed issue

--- src/discordbot/Dashboard/util/DashboarEmbedIssue.ts
+++ src/discordbot/Dashboard/util/DashboarEmbedIssue.ts
@@ -8,9 +8,15 @@ import ConvertTime from './ConvertTime';
 export default async (issue: Issue, users: User[]) => {
   const tasks = parseBody(issue.body);
 
-  console.log(tasks);
-
   const status = tasks.filter((task) => task.isComplete).length / tasks.length;
+
+  const statusLabel = issue.labels
+    .find((label) => label.name.includes('Status/'))
+    .name.split('/')[1];
+
+  const typeLabel = issue.labels
+    .find((label) => label.name.includes('Tipo/'))
+    .name.split('/')[1];
 
   const assignees = issue.assignees.map((assignee) => {
     const user = users.find((user) => user.username == assignee.username);
@@ -19,7 +25,7 @@ export default async (issue: Issue, users: User[]) => {
 
   const description =
     status > 0 && status < 100
-      ? `Ainda falta ${tasks
+      ? `Ainda faltam ${tasks
           .filter((task) => !task.isComplete)
           .map((task) => task.name)
           .join(', ')}`
@@ -28,15 +34,7 @@ export default async (issue: Issue, users: User[]) => {
   const embed = new EmbedBuilder()
     .setTitle(issue.title)
     .setDescription(description)
-    .addFields({
-      name: 'Status',
-      value: `${Math.floor(status * 100)}%`,
-    })
-    .setColor(ColorPicker(tasks))
-    .addFields({
-      name: 'Assignees',
-      value: assignees.join(', '),
-    });
+    .setColor(ColorPicker(tasks));
 
   if (issue.milestone != null) {
     const time = ConvertTime(issue.milestone.due_on);
@@ -50,6 +48,36 @@ export default async (issue: Issue, users: User[]) => {
     embed.addFields({
       name: 'Prazo',
       value: time.formattedDateTime,
+      inline: true,
+    });
+  }
+
+  embed.addFields({
+    name: 'Tipo',
+    value: typeLabel,
+  });
+
+  embed.addFields({
+    name: 'Assignees',
+    value: assignees.join(', '),
+  });
+
+  if (issue.labels.find((label) => label.name.includes('Status/'))) {
+    embed.addFields({
+      name: 'Status',
+      value: statusLabel,
+      inline: true,
+    });
+
+    embed.addFields({
+      name: 'Status',
+      value: `${Math.floor(status * 100)}%`,
+      inline: true,
+    });
+  } else {
+    embed.addFields({
+      name: 'Status',
+      value: `${Math.floor(status * 100)}%`,
       inline: true,
     });
   }
