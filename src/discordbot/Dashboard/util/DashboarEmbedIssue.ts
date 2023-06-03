5664b558454e7cf9dbb211440ee43f86a1bcad83
valmojr <val_mo_jr@hotmail.com>
2023-06-03 00:13:19 -0300

    Added missing tasks warning in dashboard

--- src/discordbot/Dashboard/util/DashboarEmbedIssue.ts
+++ src/discordbot/Dashboard/util/DashboarEmbedIssue.ts
@@ -8,6 +8,8 @@ import ConvertTime from './ConvertTime';
 export default async (issue: Issue, users: User[]) => {
   const tasks = parseBody(issue.body);
 
+  console.log(tasks);
+
   const status = tasks.filter((task) => task.isComplete).length / tasks.length;
 
   const assignees = issue.assignees.map((assignee) => {
@@ -15,8 +17,17 @@ export default async (issue: Issue, users: User[]) => {
     return user.discordId ? `<@${user.discordId}>` : user.username;
   });
 
+  const description =
+    status > 0 && status < 100
+      ? `Ainda falta ${tasks
+          .filter((task) => !task.isComplete)
+          .map((task) => task.name)
+          .join(', ')}`
+      : null;
+
   const embed = new EmbedBuilder()
     .setTitle(issue.title)
+    .setDescription(description)
     .addFields({
       name: 'Status',
       value: `${Math.floor(status * 100)}%`,
@@ -31,13 +42,15 @@ export default async (issue: Issue, users: User[]) => {
     const time = ConvertTime(issue.milestone.due_on);
 
     embed.addFields({
-      name: 'Prazo',
-      value: time.formattedDateTime,
+      name: 'Tempo restante',
+      value: `${time.daysRemaining} dias`,
+      inline: true,
     });
 
     embed.addFields({
-      name: 'Tempo restante',
-      value: `${time.daysRemaining} dias`,
+      name: 'Prazo',
+      value: time.formattedDateTime,
+      inline: true,
     });
   }
 
