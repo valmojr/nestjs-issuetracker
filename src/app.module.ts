d776bb629c20c898bd1e6a82412ae4cdc386cd05
valmojr <val_mo_jr@hotmail.com>
2023-06-07 11:42:32 -0300

    added hook module hierarchy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DiscordbotModule } from './discordbot/discordbot.module';
+import { HookModule } from './hook/hook.module';
 
 @Module({
-  imports: [UserModule, DiscordbotModule],
+  imports: [UserModule, DiscordbotModule, HookModule],
   providers: [AppService],
 })
 export class AppModule {}
